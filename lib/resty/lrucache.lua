bbb9001e85a48b000677713c8477a676e4b90b21
Yichun Zhang (agentzh) <agentzh@gmail.com>
2014-04-29 17:42:06 -0700

    initial checkin.

---
+++ lib/resty/lrucache.lua
@@ -0,0 +1,203 @@
+-- Copyright (C) Yichun Zhng (agentzh)
+
+
+local ffi = require "ffi"
+local ffi_new = ffi.new
+local ffi_sizeof = ffi.sizeof
+local ffi_cast = ffi.cast
+local ngx_now = ngx.now
+
+
+-- queue data types
+
+ffi.cdef[[
+   typedef struct lrucache_queue_s  lrucache_queue_t;
+   struct lrucache_queue_s {
+      int                ref;
+      double             expire;  /* in seconds */
+      lrucache_queue_t  *prev;
+      lrucache_queue_t  *next;
+   };
+]]
+
+local queue_arr_type = ffi.typeof("lrucache_queue_t[?]")
+local queue_ptr_type = ffi.typeof("lrucache_queue_t*")
+local queue_type = ffi.typeof("lrucache_queue_t")
+local NULL = ffi.null
+
+
+-- queue utility functions
+
+local function queue_insert_tail(h, x)
+   x.prev = h.prev
+   x.prev.next = x
+   x.next = h
+   h.prev = x
+end
+
+
+local function queue_init(size)
+   if not size then
+      size = 0
+   end
+   local q = ffi_new(queue_arr_type, size + 1)
+   ffi.fill(q, ffi_sizeof(queue_type, size + 1), 0)
+   local ptr = ffi_cast(queue_ptr_type, q)
+
+   q[0].prev = ptr
+   q[0].next = ptr
+
+   for i = 1, size do
+      queue_insert_tail(ptr, q[i])
+   end
+
+   return ptr
+end
+
+
+local function queue_is_empty(q)
+   -- print("q: ", tostring(q), "q.prev: ", tostring(q), ": ", q == q.prev)
+   return q == q.prev
+end
+
+
+local function queue_remove(x)
+   x.next.prev = x.prev
+   x.prev.next = x.next
+   x.prev = NULL
+   x.next = NULL
+end
+
+
+local function queue_insert_head(h, x)
+   x.next = h.next
+   x.next.prev = x
+   x.prev = h
+   h.next = x
+end
+
+
+local function queue_last(h)
+   return h.prev
+end
+
+
+local function queue_head(h)
+   return h.next
+end
+
+
+-- true module stuffs
+
+local _M = {}
+local mt = { __index = _M }
+
+
+local function ptr2num(ptr)
+   return tonumber(ffi_cast("uintptr_t", ptr))
+end
+
+
+function _M.new(size)
+   if size < 1 then
+      return nil, "size too small"
+   end
+
+   local self = {
+      keys = {},
+      hasht = {},
+      free_queue = queue_init(size),
+      cache_queue = queue_init(),
+      key2node = {},
+      node2key = {},
+   }
+   return setmetatable(self, mt)
+end
+
+
+function _M.get(self, key)
+   local hasht = self.hasht
+   local val = hasht[key]
+   if not val then
+      return nil
+   end
+
+   local node = self.key2node[key]
+
+   -- print(key, ": moving node ", tostring(node), " to cache queue head")
+   local cache_queue = self.cache_queue
+   queue_remove(node)
+   queue_insert_head(cache_queue, node)
+
+   if node.expire >= 0 and node.expire < ngx_now() then
+      -- print("expired: ", node.expire, " > ", ngx_now())
+      return nil
+   end
+   return val
+end
+
+
+function _M.delete(self, key)
+   self.hasht[key] = nil
+
+   local key2node = self.key2node
+   local node = key2node[key]
+
+   if not node then
+      return false
+   end
+
+   key2node[key] = nil
+   self.node2key[ptr2num(node)] = nil
+
+   queue_remove(node)
+   queue_insert_tail(self.free_queue, node)
+   return true
+end
+
+
+function _M.set(self, key, value, ttl)
+   local hasht = self.hasht
+   hasht[key] = value
+
+   local key2node = self.key2node
+   local node = key2node[key]
+   if not node then
+      local free_queue = self.free_queue
+      local node2key = self.node2key
+
+      if queue_is_empty(free_queue) then
+         -- evict the least recently used key
+         assert(not queue_is_empty(self.cache_queue))
+         node = queue_last(self.cache_queue)
+
+         local oldkey = node2key[ptr2num(node)]
+         -- print(key, ": evicting oldkey: ", oldkey, ", oldnode: ",
+         --       tostring(node))
+         if oldkey then
+            hasht[oldkey] = nil
+            key2node[oldkey] = nil
+         end
+
+      else
+         -- take a free queue node
+         node = queue_head(free_queue)
+         -- print(key, ": get a new free node: ", tostring(node))
+      end
+
+      node2key[ptr2num(node)] = key
+      key2node[key] = node
+   end
+
+   queue_remove(node)
+   queue_insert_head(self.cache_queue, node)
+
+   if ttl then
+      node.expire = ngx_now() + ttl
+   else
+      node.expire = -1
+   end
+end
+
+
+return _M
---
+++ t/lrucache.t
@@ -0,0 +1,170 @@
+# vim:set ft= ts=4 sw=4 et fdm=marker:
+
+use Test::Nginx::Socket::Lua;
+use Cwd qw(cwd);
+
+repeat_each(2);
+
+plan tests => repeat_each() * (blocks() * 3);
+
+#no_diff();
+#no_long_string();
+
+my $pwd = cwd();
+
+our $HttpConfig = <<"_EOC_";
+    lua_package_path "$pwd/lib/?.lua;;";
+    #init_by_lua '
+    #local v = require "jit.v"
+    #v.on("$Test::Nginx::Util::ErrLogFile")
+    #require "resty.core"
+    #';
+
+_EOC_
+
+no_long_string();
+run_tests();
+
+__DATA__
+
+=== TEST 1: sanity
+--- http_config eval: $::HttpConfig
+--- config
+    location = /t {
+        content_by_lua '
+            local lrucache = require "resty.lrucache"
+            local c = lrucache.new(2)
+
+            c:set("dog", 32)
+            c:set("cat", 56)
+            ngx.say("dog: ", c:get("dog"))
+            ngx.say("cat: ", c:get("cat"))
+
+            c:set("dog", 32)
+            c:set("cat", 56)
+            ngx.say("dog: ", c:get("dog"))
+            ngx.say("cat: ", c:get("cat"))
+
+            c:delete("dog")
+            c:delete("cat")
+            ngx.say("dog: ", c:get("dog"))
+            ngx.say("cat: ", c:get("cat"))
+        ';
+    }
+--- request
+    GET /t
+--- response_body
+dog: 32
+cat: 56
+dog: 32
+cat: 56
+dog: nil
+cat: nil
+
+--- no_error_log
+[error]
+
+
+
+=== TEST 2: evict existing items
+--- http_config eval: $::HttpConfig
+--- config
+    location = /t {
+        content_by_lua '
+            local lrucache = require "resty.lrucache"
+            local c = lrucache.new(2)
+            if not c then
+               ngx.say("failed to init lrucace: ", err)
+               return
+            end
+
+            c:set("dog", 32)
+            c:set("cat", 56)
+            ngx.say("dog: ", c:get("dog"))
+            ngx.say("cat: ", c:get("cat"))
+
+            c:set("bird", 76)
+            ngx.say("dog: ", c:get("dog"))
+            ngx.say("cat: ", c:get("cat"))
+            ngx.say("bird: ", c:get("bird"))
+        ';
+    }
+--- request
+    GET /t
+--- response_body
+dog: 32
+cat: 56
+dog: nil
+cat: 56
+bird: 76
+
+--- no_error_log
+[error]
+
+
+
+=== TEST 3: evict existing items (reordered, get should also count)
+--- http_config eval: $::HttpConfig
+--- config
+    location = /t {
+        content_by_lua '
+            local lrucache = require "resty.lrucache"
+            local c = lrucache.new(2)
+            if not c then
+               ngx.say("failed to init lrucace: ", err)
+               return
+            end
+
+            c:set("cat", 56)
+            c:set("dog", 32)
+            ngx.say("dog: ", c:get("dog"))
+            ngx.say("cat: ", c:get("cat"))
+
+            c:set("bird", 76)
+            ngx.say("dog: ", c:get("dog"))
+            ngx.say("cat: ", c:get("cat"))
+            ngx.say("bird: ", c:get("bird"))
+        ';
+    }
+--- request
+    GET /t
+--- response_body
+dog: 32
+cat: 56
+dog: nil
+cat: 56
+bird: 76
+
+--- no_error_log
+[error]
+
+
+
+=== TEST 4: ttl
+--- http_config eval: $::HttpConfig
+--- config
+    location = /t {
+        content_by_lua '
+            local lrucache = require "resty.lrucache"
+            local c = lrucache.new(1)
+
+            c:set("dog", 32, 0.1)
+            ngx.say("dog: ", c:get("dog"))
+
+            ngx.sleep(0.05)
+            ngx.say("dog: ", c:get("dog"))
+
+            ngx.sleep(0.051)
+            ngx.say("dog: ", c:get("dog"))
+        ';
+    }
+--- request
+    GET /t
+--- response_body
+dog: 32
+dog: 32
+dog: nil
+
+--- no_error_log
+[error]
+
